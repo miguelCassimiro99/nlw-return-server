4570a5925f31cc0b51cfb053830f0fbc3c97c72e
Miguel Cassimiro <miguel.cassimiro99@gmail.com>
2022-05-07 12:12:38 -0300

    isolating code on repositories and use-cases

--- src/server.ts
+++ src/server.ts
@@ -1,46 +1,11 @@
 import express from 'express'
-import { prisma } from './prisma'
-import nodemailer from 'nodemailer'
+
+import { routes } from './routes'
 
 const app = express()
 
 app.use(express.json())
-
-
-const transport = nodemailer.createTransport({
-  host: "smtp.mailtrap.io",
-  port: 2525,
-  auth: {
-    user: "8cddf83d1b87ce",
-    pass: "cd7769f91f843a"
-  }
-});
-
-app.post('/feedback', async (req, res) => {
-
-  const  { type, comment, screenshot } = req.body
-  const feedback = await prisma.feedback.create({
-    data: {
-      type,
-      comment,
-      screenshot
-    }
-  })
-
-  await transport.sendMail({
-    from: 'Equipe feedget <oi@feedget.com>',
-    to: 'Miguel Cassimiro <miguel.cassimiro99@gmail.com>',
-    subject: 'Novo Feedback',
-    html: [
-      '<div style="font-family: sans-serif; font-size: 16px; color: #111;">',
-      `<p>Tipo de feedback: ${type}</p>`,
-      `<p>Comentário: ${comment}</p>`,
-      '</div>'
-    ].join('\n')
-  })
-
-  return res.status(201).json({data: feedback})
-})
+app.use(routes) // contém rotas, conexão banco e envio de email
 
 app.listen(3333, () => {
   console.log('HTTP server running')
