1e886568ce924c41f5ee7f3ba59a85ef306ec0a1
Miguel Cassimiro <miguel.cassimiro99@gmail.com>
2022-05-10 07:32:16 -0300

    added env.sample, changed port and ignored dev.db

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,6 @@ app.use(cors())
 app.use(express.json())
 app.use(routes) // contém rotas, conexão banco e envio de email
 
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
   console.log('HTTP server running')
 })
